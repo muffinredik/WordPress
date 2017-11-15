43c185ea096ce6db1ba075acd9aa077b92cea5e6
muffinredik <liss.dominik@gmail.com>
2017-11-15 12:12:38 +0100

    Create plugin-template.php

---
+++ plugin-template.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Hopper - Projects and Tasks
+Version: 1.0.0
+Author: Dominik Liss
+Author URI: http://dominikliss.com
+License: GPLv2
+*/
+
+include( 'includes/custom-loops.php' );
+
+include( 'includes/register-settings.php' );
+include( 'includes/register-post-types.php' );
+include( 'includes/register-meta-boxes.php' );
