b565920cb45f7b4a35cda8f422077ab9a5b36906
muffinredik <liss.dominik@gmail.com>
2017-11-15 10:38:12 +0100

    custom post types snippet added

---
+++ custom-post-types.php
@@ -0,0 +1,32 @@
+<?php 
+
+add_action( 'init', 'hpat_register_post_types' );
+
+function hpat_register_post_types() {
+    
+    $labels = array(
+        'name'                  => 'Projects',
+        'singular_name'         => 'Project',
+        'add_new'               => 'Add New Project',
+        'add_new_item'          => 'Add New Project',
+        'edit_item'             => 'Edit Project',
+        'new_item'              => 'New Project',
+        'all_items'             => 'All Projects',
+        'view_items'            => 'View Projects',
+        'search_items'          => 'Search Projects',
+        'not_found'             => 'No projects found',
+        'not_found_in_trash'    => 'No projects found in trash',
+        'menu_name'             => 'Projects',
+    );
+    
+    $args = array(
+        'labels'        => $labels,
+        'public'        => true,
+        'has_archive'   => true,
+        'taxonomies'    => array( 'category' ),
+        'rewrite'       => array( 'slug' => 'project' ),
+        'supports'      => array( 'title' ),
+    );
+}
+
+?>
