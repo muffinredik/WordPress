24c498c5c220006c3fe942a6163fd042895b9aac
Dominik Liss <liss.dominik@gmail.com>
2018-01-28 16:49:31 +0100

    Create register-meta-boxes.php

---
+++ Tutorials/Hopper - Projects and Tasks/1 - 3/register-meta-boxes.php
@@ -0,0 +1,51 @@
+<?php
+add_action( 'add_meta_boxes', 'hpat_meta_boxes_init' );
+
+function hpat_meta_boxes_init() {
+    
+    add_meta_box( 'hpat-task-meta', __('Task Information', 'hopper-projects-and-tasks'), 
+                'hpat_task_meta_box', 'tasks', 'normal', 'default');
+}
+
+function hpat_task_meta_box( $post, $box ) {
+    
+    $hpat_task_state = get_post_meta( $post->ID, '_hpat_task_state', true );
+    $hpat_project_slug = get_post_meta( $post->ID, '_hpat_project_slug', true );
+    $hpat_projects = hpat_get_custom_post_array('projects');
+            
+    wp_nonce_field( plugin_basename( __FILE__ ), 'hpat_save_task_meta_box');
+    
+    echo '<p>' . __('Done', 'hopper-projects-and-tasks') . ': <input type="checkbox" name="hpat_task_state"';
+    if( $hpat_task_state == 'on' ){ 
+        echo 'checked '; 
+    }
+    echo '/></p>'; 
+    echo '<p>' . __('Project', 'hopper-projects-and-tasks') . ': <select name="hpat_project_slug">';
+    foreach ($hpat_projects as $hpat_project) {
+        echo '<option value="' . $hpat_project['slug'] . '" ' . selected( $hpat_project_slug, $hpat_project['slug'], false ) . '>' 
+                . $hpat_project['title'] . 
+            '</option>';
+    }
+    echo '</select>';
+}
+
+add_action( 'save_post', 'hpat_save_task_meta_box' );
+
+function hpat_save_task_meta_box( $post_id ) {
+                
+    if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+        return;
+
+    wp_verify_nonce( plugin_basename( __FILE__ ), 'hpat_save_task_meta_box' );
+    
+    if( isset( $_POST['hpat_task_state'] ) ) {
+        update_post_meta( $post_id, '_hpat_task_state', 'on' );
+    }
+    else {
+        update_post_meta( $post_id, '_hpat_task_state', '' );
+    }
+    
+    if( isset( $_POST['hpat_project_slug'] ) ) {
+        update_post_meta( $post_id, '_hpat_project_slug', $_POST['hpat_project_slug'] );
+    }
+}
