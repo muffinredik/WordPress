e4e46627f0bc038fe83b9b6602cd0be0096aaa30
Dominik Liss <liss.dominik@gmail.com>
2018-01-28 16:41:48 +0100

    Create hopper-projects-and-tasks.php

---
+++ Tutorials/Hopper - Projects and Tasks/4 -bersetzungsf-higes Entwickeln/hopper-projects-and-tasks.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Hopper - Projects and Tasks
+Version: 1.0.0
+Author: Dominik Liss
+Author URI: http://dominikliss.com
+License: GPLv2
+Text-Domain: hopper-projects-and-tasks
+*/
+
+include( 'includes/custom-loops.php' );
+
+include( 'includes/register-settings.php' ); 
+include( 'includes/register-post-types.php' );
+include( 'includes/register-meta-boxes.php' );
