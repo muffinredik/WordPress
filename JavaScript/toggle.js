4909441fa4c522046267cd7c1e62d122d398c9b0
Dominik Liss <liss.dominik@gmail.com>
2019-05-16 14:53:42 +0200

    Update toggle.js

--- JavaScript/toggle.js
+++ JavaScript/toggle.js
@@ -1,15 +1,25 @@
 
-jQuery(document).ready(function($) {
-    //$('.hpat-newsmodul-excerpt').hide();
-    
-    $('.hpat-newsmodul-title').click(function(){
-        var content = $(this).siblings('.hpat-newsmodul-excerpt');
-        if(content.is(":hidden")){
-            $('.hpat-newsmodul-excerpt').slideUp();
-            content.slideDown();
-        }
-        else {
-            content.slideUp();
-        }
+(function($){
+
+    $(window).load(function() {
+
+        $test = $('body');
+
     });
-});
+    
+    $(document).ready(function() {
+        //$('.hpat-newsmodul-excerpt').hide();
+
+        $('.hpat-newsmodul-title').click(function(){
+            var content = $(this).siblings('.hpat-newsmodul-excerpt');
+            if(content.is(":hidden")){
+                $('.hpat-newsmodul-excerpt').slideUp();
+                content.slideDown();
+            }
+            else {
+                content.slideUp();
+            }
+        });
+    });
+
+})(jQuery);
