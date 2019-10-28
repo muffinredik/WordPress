cfce012aa8025e84af67a97eeaac24998a9ade77
Dominik Liss <liss.dominik@gmail.com>
2019-10-28 13:21:06 +0100

    Create WP_Queries.php

---
+++ CustomPostTypes/WP_Queries.php
@@ -0,0 +1,18 @@
+<?php
+
+// get posts afer now
+$args = array (
+  'post_type' => 'mwd_events',
+  'order' => 'ASC',
+  'orderby' => 'meta_value',
+  'meta_key' => 'wpcf-datum_anfang',
+  'posts_per_page' => '-1',
+  'meta_query' => array( 
+    array(
+      'key' => 'wpcf-datum_anfang', 
+      'value' => current_time('U'), 
+      'compare' => '>=', 
+      'type' => 'NUMERIC' 
+    )
+  ),
+);
