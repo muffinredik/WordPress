f55ce0682cce6b375fd3cc77742ddf09e7085b5b
Dominik Liss <liss.dominik@gmail.com>
2019-11-18 15:41:46 +0100

    Update edit-options-capabilities.php

--- UserCapabilities/edit-options-capabilities.php
+++ UserCapabilities/edit-options-capabilities.php
@@ -1,6 +1,6 @@
 <?php
 
-  function wpse_310606_setting_capability( $capability ) {
+  function dl_set_capability( $capability ) {
     return 'edit_pages';
-}
-add_filter( 'option_page_capability_wpse_310606_setting_group', 'wpse_310606_setting_capability' );
+  }
+  add_filter( 'option_page_capability_[option-id]', 'dl_set_capability' );
