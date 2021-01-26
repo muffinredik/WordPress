e757a3b0da9329e7327ce300398f0d217db6209c
Dominik Liss <liss.dominik@gmail.com>
2021-01-26 13:32:09 +0800

    Update shortcodes.php

--- tutorials/shortcodes.php
+++ tutorials/shortcodes.php
@@ -1,2 +1,22 @@
 <?php 
 
+// Shortcode
+add_shortcode( 'dl-year', 'dl_year_handler' );
+
+function dl_year_handler() {
+	return date( 'Y' );
+}
+
+// Shortcode mit Parametern
+add_shortcode( 'dl-year', 'dl_year_handler' );
+
+function dl_year_handler( $atts ) {
+
+	// Default Values
+	$atts = shortcode_atts(
+			array(
+				'credit' => 'by Dominik Liss',
+		), $atts );
+
+	return date( 'Y' ) . ' - ' . $atts['credit'];
+}
